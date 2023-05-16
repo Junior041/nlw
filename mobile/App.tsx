baf590a8a90e14a0965413421f8361efad7f6cee
Junior <ismaeljunior1508@gmail.com>
2023-05-16 20:55:36 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text className='shadow-xl bg-red-700'>Hellow Word</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: 'white',
+    fontSize : 48,
+  }
+});
