03c0dac8f43470688915a5b116d89113d787ab8f
Junior <ismaeljunior1508@gmail.com>
2023-05-17 21:59:43 -0300

    40m aula3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
+import 'dotenv/config'
 import fastify from "fastify";
-import { cors } from "@fastify/cors";
+import cors from "@fastify/cors";
+import jwt from '@fastify/jwt';
 import { memoriesRoutes } from "./routes/memories";
+import { authRouter } from "./routes/auth";
 const app = fastify()
 
 app.register(memoriesRoutes)
+app.register(authRouter)
 app.register(cors,{
     origin: true, //todas as urls de fonrt-end poderao acessar nosso back-end
+})
+app.register(jwt, {
+    secret: 'spacetime'
 })
 app.listen({
     port: 3333,
